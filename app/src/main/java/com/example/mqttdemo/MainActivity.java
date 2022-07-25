3e000666523b9eecd45f20e3028102d1b8fdf1b3
kirandhiyad <dhiyadkiran07@gmail.com>
2022-07-25 22:34:05 +0530

    MQTT Demo For Android 12

--- app/src/main/java/com/example/mqttdemo/MainActivity.java
+++ app/src/main/java/com/example/mqttdemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mqttdemo;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +16,6 @@ import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import info.mqtt.android.service.Ack;
@@ -25,12 +23,10 @@ import info.mqtt.android.service.MqttAndroidClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button,button1,button2;
-    public String ServerUri = "tcp://3.251.89.203:1883";
-    public String MQTTIP = "3.251.89.203", MQTTPort = "1883";
-    public String USERNAME = "WHYTE", PASSWORD = "Whyte@123";
+    Button button, button1, button2;
+    public String ServerUri = "tcp://*Server*:*Port*";
+    public String USERNAME = "USERNAME", PASSWORD = "PASSWORD";
     public String Publisher = "1111111111_1", Topic = "1111111111";
-    String ClientId = "Karan@123";
     MqttAndroidClient mqttAndroidClient;
 
     @Override
@@ -46,12 +42,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 MQTTConnect();
-//                try {
-//                    MQTTConnect();
-//                } catch (MqttException e) {
-//                    System.out.println("not call");
-//                    e.printStackTrace();
-//                }
             }
         });
 
@@ -59,59 +49,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 subscribeToTopic();
-//                try {
-//                    MQTTConnect();
-//                } catch (MqttException e) {
-//                    System.out.println("not call");
-//                    e.printStackTrace();
-//                }
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                publishMessage("aasss");
-//                try {
-//                    MQTTConnect();
-//                } catch (MqttException e) {
-//                    System.out.println("not call");
-//                    e.printStackTrace();
-//                }
+                publishMessage("Demo Message.");
+
             }
         });
     }
 
 
-        private void MQTTConnect() {
-
-//        String clientId ="Android_111125";
+    private void MQTTConnect() {
 
         String clientId = MqttClient.generateClientId();
         System.out.println(clientId);
-        mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), ServerUri, clientId,Ack.AUTO_ACK);
+        mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), ServerUri, clientId, Ack.AUTO_ACK);
 
-        System.out.println("1");
         MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
         mqttConnectOptions.setAutomaticReconnect(true);
         mqttConnectOptions.setCleanSession(false);
         mqttConnectOptions.setUserName(USERNAME);
         mqttConnectOptions.setPassword(PASSWORD.toCharArray());
 
-        System.out.println("2");
-
 
         try {
-            System.out.println("3");
+
             IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
-            System.out.println("4");
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     Toast.makeText(MainActivity.this, "Successfully Connected To Sever.", Toast.LENGTH_SHORT).show();
                     System.out.println("Connection SuccessFul.");
-                    subscribeToTopic();
-                    publishMessage("AABB");
 
                     DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
                     disconnectedBufferOptions.setBufferEnabled(true);
@@ -125,16 +96,13 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                     Toast.makeText(MainActivity.this, "Not Connected To Server.", Toast.LENGTH_SHORT).show();
-                    System.out.println("5");
                 }
             });
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("6");
         }
 
     }
-
 
 
     private void subscribeToTopic() {
@@ -156,8 +124,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mqttAndroidClient.setCallback(new MqttCallback() {
-            private String topic;
-            private MqttMessage message;
 
             @Override
             public void connectionLost(Throwable cause) {
@@ -168,24 +134,18 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
-                System.out.println("Message2: " + topic + " : " + new String(message.getPayload()));
+                System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
 
-                if (topic.equals(Publisher)){
-                    Toast.makeText(MainActivity.this, "MQTT Operation Successful.", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Something Went Wrong.", Toast.LENGTH_SHORT).show();
-                }
             }
 
             @Override
             public void deliveryComplete(IMqttDeliveryToken token) {
-                System.out.println("Delivery Complete2"+token);
+                System.out.println("Delivery Complete" + token);
             }
         });
     }
 
-    public void publishMessage(@NonNull String MQTTCommand){
+    public void publishMessage(@NonNull String MQTTCommand) {
 
         try {
             MqttMessage message = new MqttMessage();
@@ -210,133 +170,4 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-//    private void MQTTConnect() throws MqttException {
-//
-////        String clientId ="Android_111125";
-//
-//        String clientId = MqttClient.generateClientId();
-//        System.out.println(clientId);
-//        mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), ServerUri, clientId);
-//
-//        System.out.println("1");
-//        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-//        mqttConnectOptions.setAutomaticReconnect(true);
-//        mqttConnectOptions.setCleanSession(false);
-//        mqttConnectOptions.setUserName(USERNAME);
-//        mqttConnectOptions.setPassword(PASSWORD.toCharArray());
-//
-//        System.out.println("2");
-//
-//
-//        try {
-//            System.out.println("3");
-//            IMqttToken token = mqttAndroidClient.connect(mqttConnectOptions);
-//            System.out.println("4");
-//            token.setActionCallback(new IMqttActionListener() {
-//                @Override
-//                public void onSuccess(IMqttToken asyncActionToken) {
-//                    Toast.makeText(MainActivity.this, "Successfully Connected To Sever.", Toast.LENGTH_SHORT).show();
-//                    System.out.println("Connection SuccessFul.");
-//                    subscribeToTopic();
-//                    publishMessage("AABB");
-//
-//                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
-//                    disconnectedBufferOptions.setBufferEnabled(true);
-//                    disconnectedBufferOptions.setBufferSize(100);
-//                    disconnectedBufferOptions.setPersistBuffer(false);
-//                    disconnectedBufferOptions.setDeleteOldestMessages(false);
-//                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
-//
-//                }
-//
-//                @Override
-//                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                    Toast.makeText(MainActivity.this, "Not Connected To Server.", Toast.LENGTH_SHORT).show();
-//                    System.out.println("5");
-//                }
-//            });
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            System.out.println("6");
-//        }
-//
-//    }
-
-
-
-
-//    private void subscribeToTopic() {
-//
-//        try {
-//            mqttAndroidClient.subscribe(Publisher, 1, null, new IMqttActionListener() {
-//                @Override
-//                public void onSuccess(IMqttToken asyncActionToken) {
-//                    Toast.makeText(MainActivity.this, "Subscribed Successful.", Toast.LENGTH_SHORT).show();
-//                }
-//
-//                @Override
-//                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                    Toast.makeText(MainActivity.this, "Subscribed Fail", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//        } catch (MqttException e) {
-//            e.printStackTrace();
-//        }
-//
-//        mqttAndroidClient.setCallback(new MqttCallback() {
-//            private String topic;
-//            private MqttMessage message;
-//
-//            @Override
-//            public void connectionLost(Throwable cause) {
-//                Toast.makeText(MainActivity.this, "Connection Lost", Toast.LENGTH_SHORT).show();
-//                System.out.println("Connection Lost");
-//
-//            }
-//
-//            @Override
-//            public void messageArrived(String topic, MqttMessage message) throws Exception {
-//                System.out.println("Message2: " + topic + " : " + new String(message.getPayload()));
-//
-//                if (topic.equals(Publisher)){
-//                    Toast.makeText(MainActivity.this, "MQTT Operation Successful.", Toast.LENGTH_SHORT).show();
-//                }
-//                else {
-//                    Toast.makeText(MainActivity.this, "Something Went Wrong.", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void deliveryComplete(IMqttDeliveryToken token) {
-//                System.out.println("Delivery Complete2"+token);
-//            }
-//        });
-//    }
-//
-//    public void publishMessage(@NonNull String MQTTCommand){
-//
-//        try {
-//            MqttMessage message = new MqttMessage();
-//            message.setQos(1);
-//            message.setPayload(MQTTCommand.getBytes());
-//
-//            mqttAndroidClient.publish(Topic, message, null, new IMqttActionListener() {
-//                @Override
-//                public void onSuccess(IMqttToken asyncActionToken) {
-//                    Toast.makeText(MainActivity.this, "Message Published.", Toast.LENGTH_SHORT).show();
-//                }
-//
-//                @Override
-//                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                    Toast.makeText(MainActivity.this, "Publish Message Failed", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//        } catch (MqttException e) {
-//            Toast.makeText(this, "Something Went Wrong.", Toast.LENGTH_SHORT).show();
-//            System.err.println("Error Publishing: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-//    }
 }
